c42dc6fbc1fb1bc0318b6b64301a262e8b124826
Ifeanyi <alichiifeanyi@yahoo.com>
2021-05-18 13:32:05 +0100

    more edits

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -11,16 +12,20 @@ class _LandingPageState extends State<LandingPage> {
   User _user;
 
   void _updateUser(User user) {
-    print('User id: ${user.uid}');
+    // print('User id: ${user.uid}');
+    setState(() {
+      _user = user;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     if (_user == null) {
       return SignInPage(
-        onSignIn: (user) => _updateUser(user),
+        // onSignIn: (user) => _updateUser(user),
+        onSignIn: _updateUser,
       );
     }
-    return Container(); // temporary placeholder for homepage
+    return HomePage();
   }
 }
