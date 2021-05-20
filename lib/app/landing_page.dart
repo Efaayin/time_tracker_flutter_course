6f1ac8251546589d3c83067715c9d1627646883b
Ifeanyi <alichiifeanyi@yahoo.com>
2021-05-20 10:32:04 +0100

    more edits

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -20,6 +20,9 @@ class _LandingPageState extends State<LandingPage> {
   @override
   void initState() {
     super.initState();
+    widget.auth.authStateChanges().listen((user) { 
+      print('uid: ${user?.uid}');
+    });
     _updateUser(widget.auth.currentUser);
   }
 
