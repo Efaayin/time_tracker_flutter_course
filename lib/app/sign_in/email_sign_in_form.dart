123c76abb4e489a16970b0d42ebd9e3d7955544f
Ifeanyi <alichiifeanyi@yahoo.com>
2021-05-25 13:38:01 +0100

    email auth

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+
+  String email;
+
+  void _submit() {
+    // TODO: print email and password
+
+    print(email);
+  }
+
   List<Widget> _buildChildren() {
     return [
       TextField(
@@ -9,14 +19,39 @@ class EmailSignInForm extends StatelessWidget {
           labelText: 'Email',
           hintText: 'test@test.com',
         ),
+        onChanged: (value) => email = value,
+      ),
+      SizedBox(height: 8.0),
+      TextField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+      SizedBox(height: 20.0),
+      FormSubmitButton(
+        text: 'Sign In',
+        onPressed: _submit,
+      ),
+      SizedBox(height: 8.0),
+      TextButton(
+        onPressed: () {},
+        child: Text(
+          'Need an account? Register',
+        ),
       ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _buildChildren(),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: _buildChildren(),
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      ),
     );
   }
 }
