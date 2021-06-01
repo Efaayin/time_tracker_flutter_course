d6903a4f4f20b94ab832c20bd1a9f21b2d9a4d79
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-01 12:20:31 +0100

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         title: 'Time Tracker',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
-          platform: TargetPlatform.iOS,
+          // platform: TargetPlatform.iOS,
         ),
         home: LandingPage(),
       ),
