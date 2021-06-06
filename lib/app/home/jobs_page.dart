4e3a877838e033dc0f60fb6743b6ab11eefdba95
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-07 00:15:26 +0100

    edit

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -4,10 +4,12 @@ import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dar
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class JobsPage extends StatelessWidget {
-
   Future<void> _signOut(BuildContext context) async {
     try {
-      final auth = Provider.of<AuthBase>(context, listen: false,);
+      final auth = Provider.of<AuthBase>(
+        context,
+        listen: false,
+      );
       await auth.signOut();
     } catch (e) {
       print(e.toString());
@@ -45,6 +47,10 @@ class JobsPage extends StatelessWidget {
             onPressed: () => _confirmSignOut(context),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -4,11 +4,15 @@ import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthBase>(context, listen: false,);
+    final auth = Provider.of<AuthBase>(
+      context,
+      listen: false,
+    );
     return StreamBuilder<User>(
       stream: auth.authStateChanges(),
       builder: (context, snapshot) {
@@ -17,7 +21,10 @@ class LandingPage extends StatelessWidget {
           if (user == null) {
             return SignInPage.create(context);
           }
-          return JobsPage();
+          return Provider<Database>(
+            create: (_) => FirestoreDatabase(uid: user.uid),
+            child: JobsPage(),
+          );
         }
         return Scaffold(
           body: Center(
