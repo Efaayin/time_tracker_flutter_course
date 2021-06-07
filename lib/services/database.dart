91b5c85cbff2178569d563af19366457f81c32b1
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-07 11:06:04 +0100

    edit

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,6 +5,7 @@ import 'package:time_tracker_flutter_course/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
+  Stream<List<Job>> jobsStream();
 }
 
 class FirestoreDatabase implements Database {
@@ -15,6 +16,23 @@ class FirestoreDatabase implements Database {
         path: APIPath.job(uid, 'job_abc'),
         data: job.toMap(),
       );
+
+  Stream<List<Job>> jobsStream() {
+    final path = APIPath.jobs(uid);
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map(
+      (snapshot) => snapshot.docs.map(
+        (snapshot) {
+          final data = snapshot.data();
+          return data != null ? Job(
+            name: data['name'],
+            ratePerHour: data['ratePerHour'],
+          ) : null;
+        },
+      ),
+    );
+  }
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = FirebaseFirestore.instance.doc(path);
