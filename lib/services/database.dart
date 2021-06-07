cee4e32be0153044a36ac056d9a3ff1ab054662d
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-07 12:13:24 +0100

    edit

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -17,26 +17,27 @@ class FirestoreDatabase implements Database {
         data: job.toMap(),
       );
 
-  Stream<List<Job>> jobsStream() {
-    final path = APIPath.jobs(uid);
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs.map(
-          (snapshot) {
-            final data = snapshot.data();
-            return data != null
-                ? Job(
-                    name: data['name'],
-                    ratePerHour: data['ratePerHour'],
-                  )
-                : null;
-          },
-        ).toList());
-  }
+  Stream<List<Job>> jobsStream() => _collectionStream(
+    path: APIPath.jobs(uid),
+    builder: (data) => Job.fromMap(data),
+  );
 
   Future<void> _setData({String path, Map<String, dynamic> data}) async {
     final reference = FirebaseFirestore.instance.doc(path);
     print('$path: $data');
     await reference.set(data);
+  }
+
+  Stream<List<T>> _collectionStream<T>({
+    @required String path,
+    @required T Function(Map<String, dynamic> data) builder,
+  }) {
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((snapshot) => snapshot.docs
+        .map(
+          (snapshot) => builder(snapshot.data()),
+        )
+        .toList());
   }
 }
