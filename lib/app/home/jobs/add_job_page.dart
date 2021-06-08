4b27ee042852b244711f5b0c50d3829c370881c1
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-08 12:35:27 +0100

    edit

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -17,12 +17,43 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  bool _valiadteAndSaveForm() {
+    final form = _formKey.currentState;
+
+    if(form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _submit() {
+    if (_valiadteAndSaveForm()) {
+      print('Form Saved');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text('New Job'),
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              'Save',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: _submit,
+          ),
+        ],
       ),
       body: _buildContent(),
       backgroundColor: Colors.grey[200],
@@ -36,12 +67,39 @@ class _AddJobPageState extends State<AddJobPage> {
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Placeholder(
-              fallbackHeight: 200,
-            ),
+            child: _buildForm(),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildFormChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildFormChildren() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Job Name',
+        ),
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Rate per Hour',
+        ),
+        keyboardType: TextInputType.numberWithOptions(
+          signed: false,
+          decimal: false,
+        ),
+      ),
+    ];
   }
 }
