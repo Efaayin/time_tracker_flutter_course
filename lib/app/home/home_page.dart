49a27351a1b72d1b023409c90a10cba470a65a99
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-10 16:04:17 +0100

    edit

---
+++ lib/app/home/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+  TabItem _currentTab = TabItem.jobs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
