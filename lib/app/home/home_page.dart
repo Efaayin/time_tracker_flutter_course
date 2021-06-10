ac58fd70da907be01e1ac0725749b1114f04b427
Ifeanyi <alichiifeanyi@yahoo.com>
2021-06-10 16:54:12 +0100

    edit

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home/cupertino_home_scaffold.dart';
 import 'package:time_tracker_flutter_course/app/home/tab_item.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key key }) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  
   TabItem _currentTab = TabItem.jobs;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return CupertinoHomeScaffold(
+      currentTab: _currentTab,
+      onSelectTab: (item) {},
     );
   }
 }
